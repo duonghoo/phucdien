2a6cd376b6ace111478da5190fee27c9fec4a261
DESKTOP-AV76JFA\minhm <bvminh101299@gmail.com>
2022-07-27 21:27:24 +0700

    fix

--- resources/views/mail/mail.blade.php
+++ resources/views/mail/mail.blade.php
@@ -634,7 +634,7 @@
                                     <tr>
                                       <td valign="top" class="textContent">
                                         <h3 style="color:#5F5F5F;line-height:125%;font-family:Helvetica,Arial,sans-serif;font-size:20px;font-weight:normal;margin-top:0;margin-bottom:3px;text-align:left;">{{$title}}</h3>
-                                        <div style="text-align:left;font-family:Helvetica,Arial,sans-serif;font-size:15px;margin-bottom:0;margin-top:3px;color:#5F5F5F;line-height:135%;">{{$content}}</div>
+                                        <div style="text-align:left;font-family:Helvetica,Arial,sans-serif;font-size:15px;margin-bottom:0;margin-top:3px;color:#5F5F5F;line-height:135%;">{!! $content !!}</div>
                                       </td>
                                     </tr>
                                   </table>
