383847ffe603f309f3b30cf237ccc99fe057e50d
DESKTOP-AV76JFA\minhm <bvminh101299@gmail.com>
2022-07-27 21:22:09 +0700

    fix

--- resources/views/web/block/_script.blade.php
+++ resources/views/web/block/_script.blade.php
@@ -1,4 +1,4 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-hover-dropdown/2.2.1/bootstrap-hover-dropdown.min.js"></script>
-<script src="{{asset('js/page_compile/main.min.js?v=6')}}"></script>
+<script src="/js/page_compile/main.min.js?v=6"></script>
