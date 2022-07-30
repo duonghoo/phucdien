7db2966d5091dd7a73736fab8a4306a1b6fd48a7
duonghoo <duonghoo99@gmail.com>
2022-07-30 09:43:11 +0700

    gg

--- resources/views/web/_layout.blade.php
+++ resources/views/web/_layout.blade.php
@@ -101,7 +101,7 @@
     </div>
 
     <script>
-      var chatbox = document.getElementById('fb-customer-chat'); 
+      var chatbox = document.getElementById('fb-customer-chat');
       chatbox.setAttribute("page_id", "109117821889369");
       chatbox.setAttribute("attribution", "biz_inbox");
     </script>
