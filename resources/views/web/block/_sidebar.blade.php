5eeddac107f5e3e1acce752213acbc06182ef711
duonghoo <duonghoo99@gmail.com>
2022-10-11 20:04:50 +0700

    duong push

--- resources/views/web/block/_sidebar.blade.php
+++ resources/views/web/block/_sidebar.blade.php
@@ -3,12 +3,15 @@
     <div class="blog-post">
         <!-- Search -->
         <h2>Tìm kiếm</h2>
-        <div class="input-group search-text">
-          <input type="text" class="form-control search" placeholder="Tìm kiếm..">
-          <span class="input-group-btn">
-            <button class="btn btn-default btn-search" type="button"><i class="fa fa-search"></i></button>
-          </span>
-        </div>
+        @php
+            $search = isset($_GET['product_search']) ? "value=\"$_GET[product_search]\"" : 'placeholder="Tìm kiếm.."';
+        @endphp
+        <form action="" method="get" class="input-group search-text">
+            <input type="text" class="form-control search" name="product_search" {!! $search !!} >
+            <span class="input-group-btn">
+                <button class="btn btn-default btn-search" type="submit"><i class="fa fa-search"></i></button>
+            </span>
+        </form>
         <!-- Categories -->
         <h2>Chuyên mục</h2>
         <ul class="category-post">
