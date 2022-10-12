8745425d44779cce2c894293123cb66cbf3d579d
duonghoo <duonghoo99@gmail.com>
2022-10-12 09:55:17 +0700

    d

--- resources/views/web/block/_404.blade.php
+++ resources/views/web/block/_404.blade.php
@@ -1,11 +1,10 @@
 @extends('web._layout')
 @section('main')
-    <div class="row justify-content-center">
+    <div class="row justify-content-center mt-5">
         <div class="col-12 col-lg-6 my-5 text-center">
             <h1 class="font-36 font-weight-500 mb-0">Ooops... Error 404</h1>
             <h2 class="font-20 text-grey2 my-5">Sorry, but the page you are looking for doesn't exist.</h2>
             <p class="font-14">You can go to the <a href="{{ getUrlLink('/') }}" class="border px-3 py-2 text-decoration-none view-more">HOMEPAGE</a></p>
         </div>
     </div>
-</section>
 @endsection
